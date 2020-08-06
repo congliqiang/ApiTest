623102deea6b93ae75601e8214e590d9952d0b6c
congliqiang <2785347264@qq.com>
2020-08-06 14:44:52 +0800

    生成测试代码

--- go_check/common/conf.go
+++ go_check/common/conf.go
@@ -2,7 +2,8 @@ package common
 
 const (
 	PmTestUrl   = "https://cs.jicanchu.net/manager"
-	PmAddr      = "Pm" // 城市电商平台端
-	CsAddr      = "Cs" // 城市电商商户端
+	PmAddr      = "PmToken" // 城市电商平台端
+	CsAddr      = "Cs"      // 城市电商商户端
 	SuccessCode = "100000"
+	PmToken     = "c0ee9697-9242-4dd2-a0e9-16d20b3b4a59"
 )
---
+++ tpl.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"JccApiTest/go_check/common"
+	"JccApiTest/go_check/handle"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/astaxie/beego/httplib"
+	"os"
+	"strconv"
+	"text/template"
+	"time"
+)
+
+func main() {
+	tpl, err := template.ParseFiles("go_check/check/gocheckhttp_test.tpl")
+	if err != nil {
+		panic(err)
+	}
+	br := new(bytes.Buffer)
+
+	data := make(map[string]interface{})
+	entrance := handle.Entrance("./go_check/entrance.json")
+	if len(entrance) == 0 {
+		return
+	}
+	dataFunc := ""
+	for eni, env := range entrance {
+		readData := common.ReadJson("./go_check/json/" + env.RequestDataUrl)
+		requestData := make([]map[string]string, 0)
+		err := json.Unmarshal([]byte(readData), &requestData)
+		common.CheckError(err)
+		fmt.Println(requestData)
+		for i, v := range requestData {
+			url := fmt.Sprintf("%v/%s", common.PmTestUrl, env.RequestUrl)
+			var req *httplib.BeegoHTTPRequest
+			switch env.Type {
+			case "Post":
+				req = httplib.Post(url)
+			case "Get":
+				req = httplib.Get(url)
+			default:
+				panic("数据错误")
+			}
+			switch env.Addr {
+			case common.PmAddr:
+				req.Header("PmToken", common.PmToken)
+			case common.CsAddr:
+				req.Header("Token", common.PmToken)
+			}
+			var str = ""
+			for si, sv := range v {
+				if sv != "" {
+					str += `	req.Param("` + si + `","` + sv + `")
+					`
+				}
+
+			}
+			var funcName = "TestHttpRequest"
+			funcName += strconv.Itoa(eni)
+			funcName += strconv.Itoa(i)
+			funcName += strconv.Itoa(time.Now().Nanosecond())
+			dataFunc += fmt.Sprintf(`func (s *MySuite) %s(c *C) {
+						req := httplib.%s("%s")
+						req.Header("%s", "%s")
+						`+str+`
+						outPutData := handle.HandleReq(req)
+						var code = outPutData.Code
+						PmToken = outPutData.Token
+						c.Assert(code, Equals, common.SuccessCode)
+					}
+`, funcName, env.Type, url, env.Addr, common.PmToken)
+		}
+	}
+	data["NewFunc"] = dataFunc
+	err = tpl.Execute(br, data)
+	if err != nil {
+		panic(err)
+	}
+	f, e := os.Create("./go_check/check/gocheck_test.go")
+	if e != nil {
+		panic(e)
+	}
+	defer f.Close()
+	_, _ = f.Write([]byte(br.String()))
+	//fmt.Println(br.String())
+}
